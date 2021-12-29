3f4281ab87a0e7929f167fa896ff0ef5b97fdf04
Guillaume Traub <33841027+g-traub@users.noreply.github.com>
2021-12-29 21:09:55 +0100

    fix: dotfiles variable was "not found" due to bad syntax

--- scripts/symlink_dotfiles.sh
+++ scripts/symlink_dotfiles.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 DIR=$(dirname $0)
 
-dotfiles = $(find $DIR/../dotfiles -name ".[!.]*") # List all hidden files
+dotfiles=$(find $DIR/../dotfiles -name ".[!.]*") # List all hidden files
 
 for file in $dotfiles; do
   ln -sf $(realpath $file) ~/
