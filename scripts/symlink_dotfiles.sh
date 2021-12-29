f4a2a42ff0dbe44ba824fe36550b07437fb51b91
Guillaume Traub <33841027+g-traub@users.noreply.github.com>
2021-12-29 15:46:20 +0100

    refactor: simplify symlink script  and update files directory
    - remove not-working conditionnal
    - format

--- scripts/symlink_dotfiles.sh
+++ scripts/symlink_dotfiles.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
-DIR=`dirname $0`
+DIR=$(dirname $0)
 
-for entry in `find ../$DIR -name ".[!.]*"` # List all hidden files in directory
-do
-  path=`realpath $entry`
-  if ! [ -L $filepath] # if symlink does't exist create it
-  then
-    ln -s `realpath $entry`  ~/
-  fi
+dotfiles = $(find $DIR/../dotfiles -name ".[!.]*") # List all hidden files
+
+for file in $dotfiles; do
+  ln -sf $(realpath $file) ~/
 done
