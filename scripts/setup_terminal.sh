8f90a88b8878d5a107c520e2f04e5360604e281c
Guillaume Traub <33841027+g-traub@users.noreply.github.com>
2021-12-29 21:48:58 +0100

    feat: make install_snap_packages and setup_terminal scripts executable

--- scripts/setup_terminal.sh
+++ scripts/setup_terminal.sh
@@ -4,9 +4,12 @@ gsettings set org.gnome.desktop.default-applications.terminal exec /usr/bin/term
 gsettings set org.gnome.desktop.default-applications.terminal exec-arg "-x"
 # set zsh as default shell
 chsh -s $(which zsh)
+# @TODO: check that zsh is installed
 # install oh-my-zsh
+# @TODO: check it's not already installed
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 # install spaceship-prompt
+# @TODO: check it's not already installed
 git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
 ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
 # NB: theme is already selected in .zshrc
