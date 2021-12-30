cd622f099eec37f4ac17b0a4d0bfb7bf3cd4dab7
Guillaume Traub <33841027+g-traub@users.noreply.github.com>
2021-12-30 02:09:28 +0100

    feat: add fonts-powerline package for spaceship-prompt

--- scripts/install_apt_packages.sh
+++ scripts/install_apt_packages.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-PACKAGES="code terminator vim yarn htop firefox chromium-browser curl zsh spotify-client"
+PACKAGES="code terminator vim yarn htop firefox chromium-browser curl zsh spotify-client fonts-powerline"
 
 apt-get update
 apt-get install $PACKAGES -y
