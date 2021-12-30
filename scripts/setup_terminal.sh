87a2dff9229a8827caae1a9b03f63cbaed860fc9
Guillaume Traub <33841027+g-traub@users.noreply.github.com>
2021-12-30 02:07:45 +0100

    feat: setup_terminal script functionnal

--- scripts/setup_terminal.sh
+++ scripts/setup_terminal.sh
@@ -2,14 +2,21 @@
 # set terminator as default terminal
 gsettings set org.gnome.desktop.default-applications.terminal exec /usr/bin/terminator
 gsettings set org.gnome.desktop.default-applications.terminal exec-arg "-x"
-# set zsh as default shell
-chsh -s $(which zsh)
+
 # @TODO: check that zsh is installed
+
 # install oh-my-zsh
 # @TODO: check it's not already installed
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
 # install spaceship-prompt
 # @TODO: check it's not already installed
-git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
-ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH/custom/themes/spaceship-prompt" --depth=1
+ln -s "$ZSH/custom/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH/custom/themes/spaceship.zsh-theme"
 # NB: theme is already selected in .zshrc
+
+# set zsh as default shell
+chsh -s $(which zsh)
+
+# reload terminal
+exec zsh
