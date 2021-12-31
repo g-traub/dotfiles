74014d10be7aeba4a87bd2574d9795d1fa21d639
Guillaume Traub <33841027+g-traub@users.noreply.github.com>
2021-12-31 21:29:26 +0100

    feat: script to setup nvm

---
+++ scripts/install_nvm.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# get nvm script and execute it
+curl -fsSL https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
+
+# install and use lts
+nvm install --lts
