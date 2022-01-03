51fa7e6004aa3ea8213418b740e09925fdff38ef
Guillaume Traub <33841027+g-traub@users.noreply.github.com>
2022-01-03 03:56:39 +0100

    refactor: merge nvm and npm packages scripts

--- scripts/install_nvm_and_setup_node.sh
+++ scripts/install_nvm_and_setup_node.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
+NPM_PACKAGES="corepack serve"
+
 # get nvm script and execute it
 curl -fsSL https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 
-# install and use lts
+# install and use node lts
 nvm install --lts
+
+# install global npm packages
+npm i -g $NPM_PACKAGES
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -11,8 +11,7 @@ $SCRIPTS_DIR/add_apt_repositories.sh
 sudo $SCRIPTS_DIR/install_apt_packages.sh  # run as root
 sudo $SCRIPTS_DIR/install_snap_packages.sh # run as root
 $SCRIPTS_DIR/setup_terminal.sh
-$SCRIPTS_DIR/install_nvm.sh
-$SCRIPTS_DIR/install_global_npm_packages.sh
+$SCRIPTS_DIR/install_nvm_and_setup_node.sh
 $SCRIPTS_DIR/setup_github_ssh.sh $ssh_key_name $ssh_key_comment $ssh_key_passphrase
 $SCRIPTS_DIR/setup_settings.sh
 $SCRIPTS_DIR/symlink_dotfiles.sh
