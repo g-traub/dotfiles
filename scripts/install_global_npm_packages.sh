3b3485f8422e3caa7d3f14d9f15bce17c508b26d
Guillaume Traub <33841027+g-traub@users.noreply.github.com>
2022-01-03 03:11:25 +0100

    fix: make sure nvm is loaded before using npm

--- scripts/install_global_npm_packages.sh
+++ scripts/install_global_npm_packages.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 PACKAGES="corepack serve"
 
+# Make sure nvm is loaded
+if [ -z "$NVM_DIR" ]; then
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+fi
+
 npm i -g $PACKAGES
