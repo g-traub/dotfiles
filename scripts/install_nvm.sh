8be90f1fc12ceedda323d6a14d90157583dca0ba
Guillaume Traub <33841027+g-traub@users.noreply.github.com>
2022-01-03 03:07:58 +0100

    fix: don't load nvm bash_completion

--- scripts/install_nvm.sh
+++ scripts/install_nvm.sh
@@ -4,8 +4,7 @@
 curl -fsSL https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 
 # install and use lts
 nvm install --lts
