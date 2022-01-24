54f795f67ee316334ec29e0f3d1ad46d0a19d6b2
Guillaume Traub <33841027+g-traub@users.noreply.github.com>
2022-01-24 22:45:15 +0100

    fix: remove yarn

--- scripts/install_apt_packages.sh
+++ scripts/install_apt_packages.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-PACKAGES="code terminator vim yarn htop firefox chromium-browser curl zsh spotify-client fonts-powerline expect gh"
+PACKAGES="code terminator vim htop firefox chromium-browser curl zsh spotify-client fonts-powerline expect gh"
 
 apt-get update
 apt-get install $PACKAGES -y
